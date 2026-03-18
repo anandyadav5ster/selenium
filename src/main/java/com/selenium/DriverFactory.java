224de998eabb538cb9ea853d4cb58401138636e0
Anand <36939602+anandyadav5ster@users.noreply.github.com>
2026-03-18 13:47:02 +0530

    Update DriverFactory.java

--- src/main/java/com/selenium/DriverFactory.java
+++ src/main/java/com/selenium/DriverFactory.java
@@ -10,15 +10,49 @@ import io.github.bonigarcia.wdm.WebDriverManager;
 
 public class DriverFactory {
 
-    private static WebDriver driver;
+    // private static WebDriver driver;
+// ThreadLocal container to hold the driver instance for each thread
+    private static final ThreadLocal<WebDriver> driver = new ThreadLocal<>();
+     public WebDriver getDriver() {
+        return driver.get();
+    }
 
-    public static WebDriver getDriver() {
 
-        if (driver == null) {
-            WebDriverManager.chromedriver().setup();
-            driver = new ChromeDriver();
+    @Parameters("browser")
+    @BeforeMethod
+    public void setup(@Optional("chrome") String browser) {
+        
+        // Priority: Maven CLI (-Dbrowser) > TestNG XML > Default (chrome)
+        String mvnBrowser = System.getProperty("browser");
+        if (mvnBrowser != null) {
+            browser = mvnBrowser;
         }
-        return driver;
+
+        System.out.println("Thread ID: " + Thread.currentThread().getId() + " - Starting: " + browser);
+
+        WebDriver localDriver;
+        switch (browser.toLowerCase()) {
+            case "chrome":
+                WebDriverManager.chromedriver().setup();
+                localDriver = new ChromeDriver();
+                break;
+            case "firefox":
+                WebDriverManager.firefoxdriver().setup();
+                localDriver = new FirefoxDriver();
+                break;
+            case "edge":
+                WebDriverManager.edgedriver().setup();
+                localDriver = new EdgeDriver();
+                break;
+            default:
+                throw new RuntimeException("Browser '" + browser + "' is not supported!");
+        }
+
+        // Set the driver in the ThreadLocal container for this specific thread
+        driver.set(localDriver);
+
+        getDriver().manage().window().maximize();
+        getDriver().manage().timeouts().implicitlyWait(Duration.ofSeconds(10));
     }
 
     @AfterMethod
